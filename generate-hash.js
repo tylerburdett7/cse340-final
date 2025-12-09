697edde578f1c86671b6971e766e1256801e45f9
Tyler Burdett <bur23034@byui.edu>
2025-12-09 13:42:28 -0700

    moved views into src

--- generate-hash.js
+++
@@ -1,7 +0,0 @@
-import bcrypt from 'bcrypt';
-
-(async () => {
-  const hash = await bcrypt.hash('password123', 10);
-  console.log('Bcrypt hash for password123:');
-  console.log(hash);
-})();
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const { default: router } = await import("./src/controllers/routes.js");
 
   // Template engine
   app.set("view engine", "ejs");
-  app.set("views", path.join(__dirname, "views"));
+  app.set("views", path.join(__dirname, "src/views"));
 
   // Static files
   app.use(express.static(path.join(__dirname, "public")));
